ce5357191891c8ec8f3890210baaf75ed3c41dc1
quannguyen2306-ctrl <65434894+quannguyen2306-ctrl@users.noreply.github.com>
2021-08-14 09:29:29 +0700

    front-end (14/8)

--- autosolarsystem_files/assets/js/App.js
+++ autosolarsystem_files/assets/js/App.js
@@ -1,9 +1,1 @@
 
-$(document).ready(function() {
-	console.log("A")
-    const clientBtn = document.getElementById("client")
-	clientBtn.addEventListener("click", function (){
-        window.location.href="./index.html"
-    })
-});
-
